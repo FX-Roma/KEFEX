1a16c07c324da8174946ce2825e0fc40dde06052
FX-Roma <146610313+FX-Roma@users.noreply.github.com>
2026-09-16 16:33:44 -0500

    back-end seperated and sent to another repository

--- BACK-END/src/index.js
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import "./connection.js";
-
-import servidorKefex from "./server.js";
-
-servidorKefex.listen(3005, ()=>{
-    console.log("Kefex Conectado a http://localhost:3005");
-});
